a33464dab403ca40df60691eff05b5321f7a388b
Serhii <serezatuma@gmail.com>
2025-03-25 14:27:53 +0100

    Base header and footer

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+  include("partipicals/header.php");
+?>
+
+  <main>
+
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+    <h1>content will be here</h1>
+
+    <h2>by Tumarkin Serhii</h2>
+
+  </main>
+
+<?php
+  include("partipicals/footer.php");
+?>
---
+++ partipicals/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CruiseCompany</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+  <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <a href="/CruiseCompany/index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-luggage" viewBox="0 0 16 16">
+        <path d="M2.5 1a.5.5 0 0 0-.5.5V5h-.5A1.5 1.5 0 0 0 0 6.5v7a1.5 1.5 0 0 0 1 1.415v.335a.75.75 0 0 0 1.5 0V15H4v-1H1.5a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5V7h1v-.5A1.5 1.5 0 0 0 6.5 5H6V1.5a.5.5 0 0 0-.5-.5zM5 5H3V2h2z"/>
+        <path d="M3 7.5a.5.5 0 0 0-1 0v5a.5.5 0 0 0 1 0zM11 6a1.5 1.5 0 0 1 1.5 1.5V8h2A1.5 1.5 0 0 1 16 9.5v5a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 14.5v-5A1.5 1.5 0 0 1 6.5 8h2v-.5A1.5 1.5 0 0 1 10 6zM9.5 7.5V8h2v-.5A.5.5 0 0 0 11 7h-1a.5.5 0 0 0-.5.5M6 9.5v5a.5.5 0 0 0 .5.5H7V9h-.5a.5.5 0 0 0-.5.5m7 5.5V9H8v6zm1.5 0a.5.5 0 0 0 .5-.5v-5a.5.5 0 0 0-.5-.5H14v6z"/>
+      </svg>
+    </a>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+      <li><a href="/CruiseCompany/index.php" class="nav-link px-2 link-secondary">Home</a></li>
+      <li><a href="/CruiseCompany/index.php" class="nav-link px-2 link-dark">Cruises</a></li>
+      <li><a href="/CruiseCompany/index.php" class="nav-link px-2 link-dark">Pricing</a></li>
+      <li><a href="/CruiseCompany/index.php" class="nav-link px-2 link-dark">FAQs</a></li>
+      <li><a href="/CruiseCompany/index.php" class="nav-link px-2 link-dark">About US</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+      <button type="button" class="btn btn-outline-primary me-2">Login</button>
+      <button type="button" class="btn btn-primary">Sign-up</button>
+    </div>
+  </header>
