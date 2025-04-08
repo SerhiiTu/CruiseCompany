5dea04d8653ea9f7e2efc811372372a2abe0a666
Serhii <serezatuma@gmail.com>
2025-04-08 14:49:09 +0200

    minor changes

--- _inc/functions.php
+++ _inc/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function print_form_data(){
+function print_form_data() {
     foreach($_POST as $field => $value){
         echo '<h5 class="text-left">' . $field . ': ' . $value . '</h5>';
     }
--- contact.php
+++ contact.php
@@ -10,10 +10,12 @@
         <label for="name">Your name</label>
         <input type="text" class="form-control" id="name" name="name" placeholder="Serhii" required>
     </div>
+
     <div class="form-group">
         <label for="email">Your email</label>
         <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
     </div>
+
     <div class="form-group">
         <label for="counrty">Your Country</label>
         <select class="form-control" id="country" name="country">
@@ -24,16 +26,19 @@
         <option>USA</option>
         </select>
     </div>
+
     <div class="form-group">
         <label for="notes">Additional notes</label>
         <textarea class="form-control" id="notes" name="notes" rows="3"></textarea>
     </div>
+
     <div class="form-check">
         <input class="form-check-input" type="checkbox" value="true" id="accept" name="accept" required>
         <label class="form-check-label" for="accept">
             Accept to data processing
         </label>
     </div>
+    
     <button type="submit" class="btn btn-primary">Send</button>
     </form>
 
